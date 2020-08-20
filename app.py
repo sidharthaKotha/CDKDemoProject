2e9b847ecef7c70b54ad0fd51af03cf959c42a87
Kotha <sidhkoth@38f9d349d3b0.ant.amazon.com>
2020-08-20 12:37:15 -0400

    commiting new changes

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 
 from aws_cdk import core
 
-from cdk_project.cdk_project_stack import CdkProjectStack
-
+from cdk_project.cdk_project_stack import MyCrossAccountPipelineStack
 
 app = core.App()
-CdkProjectStack(app, "cdk-project")
-
+env_US = core.Environment(account=app.node.try_get_context('dev')['account'], 
+                          region=app.node.try_get_context('dev')['region'])
+env_EU = core.Environment(account=app.node.try_get_context('prod')['account'], 
+                          region=app.node.try_get_context('prod')['region'])
+MyCrossAccountPipelineStack(app, "myDevStack", env=env_US)
+MyCrossAccountPipelineStack(app, "myProdStack", isProd=True, env=env_EU)
 app.synth()
--- cdk_project/cdk_project_stack.py
+++ cdk_project/cdk_project_stack.py
@@ -3,16 +3,21 @@ from aws_cdk import (
     core
 )
 
-class CdkProjectStack(core.Stack):
+class MyCrossAccountPipelineStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str, isProd=False, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
-        _s3.Bucket(
-            self,
-            "myCDKBucketLogicalID",
-            bucket_name="mynewcdkbucket",
-            versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
-        )
+        if isProd:
+            _s3.Bucket(
+                self,
+                "myProdArtifactBucket",
+                bucket_name="mynewcdkprodbucket",
+                versioned=True
+            )
+        else:
+            _s3.Bucket(
+                self,
+                "myDevArtifactBucket"
+            )
